4d551af4b710219d17c36fed2c39bfee7a85f1d0
Banny Solano <solano.banny@gmail.com>
2016-02-20 13:24:23 -0500

    cuarta version

--- primeraparte.php
+++ primeraparte.php
@@ -5,7 +5,7 @@
   <body>
     <p>hola mundo</p>
     <a href="http://www.google.com">GOOGLE</a><br />
-    <img src="imagen.jpg" width="500" height="300"/>
+    <img src="sc.jpg" width="500" height="300"/>
    </body>
    
   </html>
