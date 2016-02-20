33227e478b86cc2bef1999328e5ac4e857107274
Banny Solano <solano.banny@gmail.com>
2016-02-20 13:03:55 -0500

    tercera version

--- primeraparte.php
+++ primeraparte.php
@@ -3,8 +3,9 @@
    <title> Bienvienido  </title>
   </head>
   <body>
-   <p>hola mundo</p>
-    <a href="http://www.google.com">GOOGLE</a>
+    <p>hola mundo</p>
+    <a href="http://www.google.com">GOOGLE</a><br />
+    <img src="imagen.jpg" width="500" height="300"/>
    </body>
    
   </html>
